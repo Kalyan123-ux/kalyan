02062c2122de0b222b03d7dd96e33ce4c8f0a5bf
kalyan123-ux <baduguakhil2004@gmail.com>
2024-07-09 15:41:20 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,48 +1,30 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const {ApolloServer,gql} = require('apollo-server-express');
+const mongoose = require('mongoose')
+const {ApolloServer,gql } = require('apollo-server-express');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-
-const app = express();
-const port = 3001
+const cors = require('cors')//import cors
+const userApiFromRouter = 
+require('./routes/userRoutes') //import
+const app = express() 
+const port = 3001;
 const url= 'mongodb+srv://baduguakhil2004:lFEAjh3yuv8VD1mG@cluster0.w5qqa6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-app.use(express.json())//parsing
-mongoose.connect(url,{useNewUrlParse:true,useUnifiedTopology:true}).then(()=>{console.log('DB connected')})
-.catch((err)=>{console.log(err)});
-//start my apollo Express server 
-const server = new ApolloServer({typeDefs,resolvers});
-async function  StartServer(){
-    await server.start();
-    server.applyMiddleware({app});//run my express code
-    app.listen(port,()=>{
-        console.log('server live 3001');
-    })
-}
-app.get('/user/search/:name',async(req,res)=>{
-    try{
-        const name=req.params.name;
-        const {data,error}=await server.executeOperation({
-            query:gql`query{ searchUser(name:${name}){id name email}}`
-        });
-        if(error){return res.status(500).send(error)}
-        res.status(201).send(data)
+app.use(express.json())
+app.use(cors()) //using cors
+mongoose.connect(url,{useNewUrlParser:true,
+useUnifiedTopology:true})
+.then(()=>{})
+.catch((err)=>{})
 
-    }catch(err){
-        res.status(500).send(err);
-    }
-})
-async function startServer() {
-    await server.start();
-    server.applyMiddleware({ app });
-    app.listen(port, () => {
-      console.log(Server is running on port ${port});
-    });
-  }
-  function TESTING(){
-    return 0;
-  }
-  TESTING()
-  
-  startServer();
-   
+const server = new ApolloServer({typeDefs,resolvers});
+app.use('/users',userApiFromRouter);//add router
+
+async function StartServer(){
+   await server.start();
+   server.applyMiddleware({app});
+   app.listen(port,()=>{
+    console.log('Server Live 3001');
+   })
+}
+
+StartServer();
---
+++ server/test/api.js
@@ -0,0 +1,7 @@
+async function fetchData(){
+    return{
+        getAllUsers:[{"id":"a","name":"a","email":"p","password":"d"}]
+    }
+
+};
+module.exports=fetchData;
